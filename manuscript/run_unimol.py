307e1445814ff58549fd5fc4b290234914ca2bfe
Helen Schmid <Helen Schmid>
2025-05-28 10:07:33 -0700

    Run unimol on substrates of promiscuous esterase dataset

--- manuscript/run_unimol.py
+++ manuscript/run_unimol.py
@@ -5,8 +5,8 @@ from enzymetk.save_step import Save
 import pandas as pd
 
 # run in enzymetk environemnt
-df_mmseqs2_esm2 = pd.read_pickle('/nvme2/helen/masterthesis/manuscript/esm2/promiscuous_esterases_mmseqs2_esm2.pkl')
+df_mmseqs2_esm2 = pd.read_pickle('/nvme2/helen/masterthesis/manuscript/promiscuous_esterases_mmseqs2_esm2.pkl')
 smiles_col = 'substrates_split'
 output_dir = 'unimol/'
 
-df_mmseqs2_esm2 << (UniMol(smiles_col) >> Save(f'{output_dir}promiscuous_esterases_unimol_embedded.pkl'))
+df_mmseqs2_esm2 << (UniMol(smiles_col) >> Save(f'{output_dir}promiscuous_esterases_unimol_embedded_v2.pkl'))
